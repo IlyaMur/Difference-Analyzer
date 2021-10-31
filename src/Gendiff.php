1e6ab78444c59f10e0606ae9aa2ba05a738e11a4
Ilya Mur <ilyamur@hotmail.com>
2021-10-31 16:22:41 +0300

    add linter badge && fix lint-warnings

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -42,22 +42,24 @@ function makeDiff(array $before, array $after)
     $unionKeys = array_unique(array_merge(array_keys($before), array_keys($after)));
     sort($unionKeys);
 
-    return array_map(function ($key) use ($before, $after) {
-        if (array_key_exists($key, $before) && array_key_exists($key, $after)) {
-            if (is_array($before[$key]) && is_array($after[$key])) {
-                $node =  buildNode('nested', $key, null, null, makeDiff($before[$key], $after[$key]));
-            } elseif ($before[$key] === $after[$key]) {
-                $node = buildNode("unchanged", $key, $before[$key], $after[$key]);
-            } else {
-                $node = buildNode("changed", $key, $before[$key], $after[$key]);
+    return array_map(
+        function ($key) use ($before, $after) {
+            if (array_key_exists($key, $before) && array_key_exists($key, $after)) {
+                if (is_array($before[$key]) && is_array($after[$key])) {
+                    $node =  buildNode('nested', $key, null, null, makeDiff($before[$key], $after[$key]));
+                } elseif ($before[$key] === $after[$key]) {
+                    $node = buildNode("unchanged", $key, $before[$key], $after[$key]);
+                } else {
+                    $node = buildNode("changed", $key, $before[$key], $after[$key]);
+                }
             }
-        }
-        if (array_key_exists($key, $before) && !array_key_exists($key, $after)) {
-            $node = buildNode("removed", $key, $before[$key], null);
-        }
-        if (!array_key_exists($key, $before) && array_key_exists($key, $after)) {
-            $node = buildNode("added", $key, null, $after[$key]);
-        }
-        return $node;
-    }, $unionKeys);
+            if (array_key_exists($key, $before) && !array_key_exists($key, $after)) {
+                $node = buildNode("removed", $key, $before[$key], null);
+            }
+            if (!array_key_exists($key, $before) && array_key_exists($key, $after)) {
+                $node = buildNode("added", $key, null, $after[$key]);
+            }
+            return $node;
+        }, $unionKeys
+    );
 }
