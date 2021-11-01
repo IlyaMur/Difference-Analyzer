457dfdf55ba4d130996c146528208562ba7d5439
Ilya Mur <ilyamur@hotmail.com>
2021-11-01 21:50:03 +0300

    fix lint

--- src/Parsers.php
+++ src/Parsers.php
@@ -8,9 +8,9 @@ function parse($rawData, $type)
 {
     $mapping = [
         'yml' =>
-            fn($rawData) => Yaml::parse($rawData, Yaml::PARSE_OBJECT_FOR_MAP),
+        fn ($rawData) => Yaml::parse($rawData, Yaml::PARSE_OBJECT_FOR_MAP),
         'json' =>
-            fn($rawData) => json_decode($rawData),
+        fn ($rawData) => json_decode($rawData, null, 512, JSON_THROW_ON_ERROR)
     ];
     return $mapping[$type]($rawData);
 }
